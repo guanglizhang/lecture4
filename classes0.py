fc2af0a51ce93c36e527710687b516f203f1b026
Guangli Zhang <guanglizhang@ucsb.edu>
2020-04-20 12:15:51 -0700

    added explantions of __init__ method

--- classes0.py
+++ classes0.py
@@ -4,3 +4,10 @@ class Flight:
         self.origin = origin
         self.destination = destination
         self.duration = duration
+
+
+# __init__ is a ‘method’, which is a function performed on individual objects. 
+# __init__ in particular is a special, built-in method that describes what should happen when a flight object is created.
+# Generally, methods take self as their first argument. self refers to the object being worked with. 
+# The other three arguments are simply the information that should be stored about a particular flight. 
+# That information is stored as ‘properties’ inside the object, using dot notation.
